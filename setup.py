081162c5aba7a0d82fc04cef526be42ff711d88a
Lane Haury <lhaury@gmail.com>
2020-01-09 22:38:37 -0800

    initial commit of plugin

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='circum_walabot',
+    version_format='{tag}',
+    author="Lane Haury",
+    author_email="lane@lumineerlabs.com",
+    description="Walabot sensor plugin for circum.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/LumineerLabs/circum-walabot",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        'circum',
+        'click',
+        'setuptools-git-version'
+    ],
+    entry_points={
+        'circum.plugins': [
+            'walabot=circum_walabot.walabot:walabot'
+        ]
+    },
+)
